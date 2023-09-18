b650692fea8af840fefe8d481d45b98c235170d7
jagmeet <65348448+jagmeet640@users.noreply.github.com>
2023-09-18 14:26:26 +0400

    updated

--- Web-dev-1/index.js
+++ Web-dev-1/index.js
@@ -29,8 +29,10 @@ document.addEventListener("DOMContentLoaded", function(){
 
 // Create a new element to display the activity information
 const activityItem = document.createElement("div");
-activityItem.classList.add("activity-item");
-activityItem.innerHTML = `<<h3>${activityTitle}</h3><p>${description}</p>`;
+activityItem.setAttribute("class","activity-show")
+activityItem.classList.add("activity-show");
+activityItem.innerHTML = `${activityTitle}`;
+
 
 // Append the new activity item to the side navigation
 sideNav.appendChild(activityItem);
