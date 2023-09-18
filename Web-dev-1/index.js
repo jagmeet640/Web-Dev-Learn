c5e2ba7772e78bef37f321e4678faafa745d06a8
jagmeet <65348448+jagmeet640@users.noreply.github.com>
2023-09-18 12:28:10 +0400

    Update index.js

--- Web-dev-1/index.js
+++ Web-dev-1/index.js
@@ -22,10 +22,6 @@ document.addEventListener("DOMContentLoaded", function(){
 
         form.reset()
 
-
-
-        
-
     });
 });
 
@@ -34,7 +30,7 @@ document.addEventListener("DOMContentLoaded", function(){
 // Create a new element to display the activity information
 const activityItem = document.createElement("div");
 activityItem.classList.add("activity-item");
-activityItem.innerHTML = `<h3>${activityTitle}</h3><p>${description}</p>`;
+activityItem.innerHTML = `<<h3>${activityTitle}</h3><p>${description}</p>`;
 
 // Append the new activity item to the side navigation
 sideNav.appendChild(activityItem);
