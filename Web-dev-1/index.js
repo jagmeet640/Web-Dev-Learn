a749cbb9c8da8463a6f22e07fbbc686f5ae2753f
jagmeet <65348448+jagmeet640@users.noreply.github.com>
2023-09-18 12:20:07 +0400

    Update index.js

--- Web-dev-1/index.js
+++ Web-dev-1/index.js
@@ -1,1 +1,21 @@
 console.log("linked properly")
+
+document.addEventListener("DOMContentLoaded", function(){
+    const form = document.querySelector(".Activity-Form")
+    const sideNav = document.querySelector(".side-nav")
+    document.addEventListener("submit", function(event){
+        event.preventDefault()
+
+        // Get the values from the form inputs
+        const activityTitle = document.querySelector('input[name="Activity-title"]').value;
+        const description = document.querySelector('input[name="Description"]').value;
+
+
+        console.log(activityTitle)
+        console.log(description)
+
+        form.reset()
+        
+
+    });
+});
