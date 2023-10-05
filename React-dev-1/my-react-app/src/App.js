302676a88083f2014e03eb44e1be94cc01c8bfc9
jagmeet640 <jagmeetghotra2002@gmail.com>
2023-10-05 20:22:15 +0400

    made changes for bootstrap

--- React-dev-1/my-react-app/src/App.js
+++ React-dev-1/my-react-app/src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
+// import './App.css';
 import ListGroup from './Components/ListGroup';
-
+import 'bootstrap/dist/css/bootstrap.css'
 function App() {
   const name = 'jagmeet';
   return (
