c558e6f786cb5df3e432481935d860f249b74c85
jagmeet640 <jagmeetghotra2002@gmail.com>
2023-10-05 20:17:08 +0400

    Update App.js

--- React-dev-1/my-react-app/src/App.js
+++ React-dev-1/my-react-app/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import ListGroup from './Components/ListGroup';
 
 function App() {
   const name = 'jagmeet';
   return (
-    <>
-    </>
+    <div>
+      <ListGroup/>
+    </div>
   );
 }
 
